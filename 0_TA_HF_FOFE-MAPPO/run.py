1b8f6e465e277f2c9dd9861b6bc54098400ef53d
Cem Celikbas <cem.celikbas@NLR.nl>
2026-05-18 14:14:48 +0200

    before jamming directivity

--- 0_TA_HF_FOFE-MAPPO/run.py
+++ 0_TA_HF_FOFE-MAPPO/run.py
@@ -196,7 +196,7 @@ def main() -> None:
 
     if not args.no_animate:
         try:
-            for _ in range(10):
+            for _ in range(3):
                 if cfg.ext.use_hf_radar:
                     tester = HFTestRunner(
                         policy, env_cfg=cfg.env, hf_cfg=cfg.ext.hf_radar,
